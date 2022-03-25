fcedc86466844b0b80d5c96dc8d31608c5a1de86
Mike Hemberger <mike@thestizmedia.com>
2022-03-25 15:21:42 -0400

    0.3.1

--- mai-galleries.php
+++ mai-galleries.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Mai Galleries
  * Plugin URI:      https://bizbudding.com/mai-design-pack/
  * Description:     Responsive image galleries with optional image lightbox.
- * Version:         0.3.0
+ * Version:         0.3.1
  *
  * Author:          BizBudding
  * Author URI:      https://bizbudding.com
@@ -89,7 +89,7 @@ final class Mai_Galleries_Plugin {
 
 		// Plugin version.
 		if ( ! defined( 'MAI_GALLERIES_VERSION' ) ) {
-			define( 'MAI_GALLERIES_VERSION', '0.3.0' );
+			define( 'MAI_GALLERIES_VERSION', '0.3.1' );
 		}
 
 		// Plugin Folder Path.
